19f5e828b0fc976c5448b98a042eb71f1cd36542
johnwesh10 <118889839+johnwesh10@users>
2022-12-09 00:41:29 -0800

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * return always 0(success)
+ */
+int main(void)
+{ 
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
+
+
+}
